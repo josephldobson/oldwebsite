7b1661cace6da1a5988ee835cb3bebe9be264c3c
josephldobson <josephliamdobson@gmail.com>
2022-06-22 18:44:03 +0100

    Testing websites

--- script.js
+++ script.js
@@ -1,3 +1,27 @@
 const canvas = document.getElementById('canvas1')
 const ctx = canvas.getContext('2d')
-console.log(canvas)
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+window.addEventListener('resize', function(){
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+});
+
+const mouse = {
+    x: null,
+    y: null,
+}
+
+canvas.addEventListener('click',function(event){
+    mouse.x = event.x;
+    mouse.y = event.y;
+    drawCircle();
+})
+
+function drawCircle(){
+    ctx.fillStyle = 'light blue';
+    ctx.beginPath();
+    ctx.arc(mouse.x,mouse.y,canvas.width/20,0,Math.PI * 2);
+    ctx.fill();
+}
